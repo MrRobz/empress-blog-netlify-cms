25a4c4c9fd6feb61949ffbaffe2594d4c4f49607
MalayaliRobz <robinphilipthomas@outlook.in>
2020-06-23 22:46:24 +0530

    options add

--- index.js
+++ index.js
@@ -3,50 +3,64 @@ const YAML = require('yaml');
 const writeFile = require('broccoli-file-creator');
 const mergeTrees = require('broccoli-merge-trees');
 const { readFileSync } = require('fs');
+const netlifyIndexGenerator = require('./netlify-templates/index');
 
 module.exports = {
   name: require('./package').name,
 
-  treeForPublic() {
+  getAddonOptions() {
     let app = this._findHost();
     let options = typeof app.options === 'object' ? app.options : {};
-    let addonConfig = options['empress-blog-netlify-cms'] || {};
+    
+    return options['empress-blog-netlify-cms'] || {};
+  },
 
+  treeForPublic() {
+    let addonConfig = this.getAddonOptions();
+    let publicPath = addonConfig.publicPath || 'admin';
+    let modulePath = addonConfig.modulePath;
+    
     let netlifyConfigJson = YAML.parse(readFileSync('./netlify-templates/config.yml', 'utf8'));
     Object.assign(netlifyConfigJson, addonConfig['netlify-config'] || {});
     let netlifyConfigOutputYml = YAML.stringify(netlifyConfigJson);
 
     const netlifyConfigTree = writeFile(
-      'admin/config.yml',
+      `${publicPath}/config.yml`,
       netlifyConfigOutputYml
     );
+    
     const netlifyIndexHtmlTree = writeFile(
-      'admin/index.html',
-      readFileSync('./netlify-templates/index.html', 'utf8')
+      `${publicPath}/index.html`,
+       netlifyIndexGenerator(modulePath)
     );
 
     return mergeTrees([netlifyConfigTree, netlifyIndexHtmlTree]);
   },
 
   contentFor(type) {
-    if (type === 'head-footer') { 
-      return '<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>';
-    }
+    let addonConfig = this.getAddonOptions();
+    let publicPath = addonConfig.publicPath || 'admin';
 
-    if (type === 'body-footer') {
-      return (`
-        <script>
-          if (window.netlifyIdentity) {
-            window.netlifyIdentity.on("init", user => {
-              if (!user) {
-                window.netlifyIdentity.on("login", () => {
-                  document.location.href = "/admin/";
-                });
-              }
-            });
-          }
-        </script>
-      `);
+    if (!addonConfig.disableIdentityScriptInIndex) {
+      if (type === 'head-footer') { 
+        return '<script defer src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>';
+      }
+  
+      if (type === 'body-footer') {
+        return (`
+          <script>
+            if (window.netlifyIdentity) {
+              window.netlifyIdentity.on("init", user => {
+                if (!user) {
+                  window.netlifyIdentity.on("login", () => {
+                    document.location.href = "/${publicPath}/";
+                  });
+                }
+              });
+            }
+          </script>
+        `);
+      }
     }
   }
 };
